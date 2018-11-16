7bb877d40c9168c17da2e7c27287a3536c4d8ba8
Lanting Guo <guolanting@mlink.cc>
2018-11-16 11:06:56 +0800

    tree code

---
+++ tree/tree.py
@@ -0,0 +1,39 @@
+
+class Node(object):
+
+    def __init_(self, id, parent_id, children_ids):
+        self._id = id
+        self._parent_id = parent_id
+        self._children_ids = children_ids
+        
+    @property
+    def id(self):
+        return self._id;
+    
+    @property
+    def parent(self):
+        return self._parent_id
+
+    @property
+    def children(self):
+        return self._children_ids
+
+
+class Tree(object):
+
+    def __init__(self, nodes):
+        # nodes is a list of node
+        # sort nodes to enable binary search
+        self._sorted_nodes = sorted(nodes, key=lambda node: node.id)
+
+    def search(self, node_id):
+        # TODO binary search
+        idx = self._sorted_nodes.index(node_id)
+        children = self.sorted_nodes[idx].children
+        children_ids = list(map(lambda child: child.id), children)
+        return children_ids
+        
+    
+    
+
+
