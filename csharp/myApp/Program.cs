b9ee0470c640cde0ae682706df044df5336ff062
Lanting Guo <guolanting@mlink.cc>
2018-11-26 17:02:01 +0800

    csharp

---
+++ csharp/myApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    int a = 18;
+	    int b = 6;
+	    int c = a + b;
+	    var list = new List<string> {"AA", "BB", "CC"};
+	    foreach (var name in list)
+	    {
+		Console.WriteLine($"Hello World! {name}");
+	    }
+        }
+    }
+}
