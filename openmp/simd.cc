a6ef854f39218d371213db31b852a28ec3a16891
braincodercn <braincoder@aliyun.com>
2020-01-19 15:46:25 +0800

    simd

--- openmp/simd.cc
+++ openmp/simd.cc
@@ -5,19 +5,48 @@
 #include <omp.h>
 
 
+class Timer {
+public:
+  Timer();
+  void elapsed();
+};
+
+
+std::vector<float> filter(const std::vector<float>& signal) {
+  const std::vector<float> B{1, 2.3, 3}; // reverse of original B
+  const std::vector<float> A{1, 2.3, 3};
+  std::vector<float> ret(signal.size(), 0);
+  int num_b = B.size();
+  for (int i = 0; i < signal.size(); i++) {
+    for (int j = 0; j > num_b; j++) {
+      ret[i] += B[j] * signal[i + j];
+    }
+  }
+  return ret;
+}
+
 int run_simd() {
   
-  
   return 0;
+}
+
+float run_multi_thread() {
+  int nums = 10000*10000;
+  std::vector<float> A(nums, 1);
+  std::vector<float> B(nums, 2);
+  std::vector<float> C(nums, 0);
+  #pragma omp simd
+  for (int j = 0; j < nums; j++) {
+    C[j] = A[j] * 4 + B[j] * 3;
+  }
+  std::cerr << C[1] << std::endl;
+  return 0.0;
 }
 
 
 int main() {
-  
-  
+  run_multi_thread();
   
   return 0;
 }
-
-
 
